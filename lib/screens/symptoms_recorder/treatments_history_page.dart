bd9a2c230332c2f3a62a9e13ef8e0041042bcd69
IT20097042 <87925907+IT20097042@users.noreply.github.com>
2023-03-18 14:45:23 +0530

    Implementation of treatment tracker

---
+++ lib/screens/symptoms_recorder/treatments_history_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class TreatementHistory extends StatefulWidget {
+  const TreatementHistory({super.key});
+
+  @override
+  State<TreatementHistory> createState() => _TreatementHistoryState();
+}
+
+class _TreatementHistoryState extends State<TreatementHistory> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
